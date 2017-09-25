8a2d98174925bd77471c329d586596256ef76d5a
oousterman <oousterman@yahoo.com>
2017-09-25 11:40:20 -0700

    Added RCO to counter, Finished putting in the segment display and routing all signals

--- CPE_233_Olive and Andrew/CPE_233_Olive and Andrew.srcs/sources_1/new/Counter_4Bit.vhd
+++ CPE_233_Olive and Andrew/CPE_233_Olive and Andrew.srcs/sources_1/new/Counter_4Bit.vhd
@@ -4,26 +4,31 @@ use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity Counter_4Bit is
-  port (  RESET : in std_logic;
-                CLK : in std_logic;
-                LD : in std_logic;
-                EN : in std_logic; 
-                DIN : in std_logic_vector (3 downto 0); 
-                COUNT : out std_logic_vector (3 downto 0)); 
+  port (    RESET : in std_logic;
+            CLK : in std_logic;
+            LD : in std_logic;
+            EN : in std_logic; 
+            DIN : in std_logic_vector (3 downto 0); 
+            COUNT : out std_logic_vector (3 downto 0);
+            RCO : out std_logic);
 end Counter_4Bit; 
 
 architecture Behavioral of Counter_4Bit is 
-   signal  t_cnt : std_logic_vector(3 downto 0); 
+   signal  t_cnt : std_logic_vector(3 downto 0);
+   signal  OVERFLOW : std_logic;
 begin 
          
-   process (CLK, RESET, LD, EN, t_cnt) 
+   process (CLK, RESET, LD, EN, t_cnt, OVERFLOW) 
    begin
       if (RESET = '1') then    
          t_cnt <= (others => '0'); -- async clear
       elsif (rising_edge(CLK)) then
+         OVERFLOW <='0';
          if (LD = '1') then     t_cnt <= DIN;  -- load
          else 
-            if (EN = '1') then  t_cnt <= t_cnt + 1; -- incr
+            if (EN = '1') then  t_cnt <= t_cnt + 1;
+                if(t_cnt = "1111") then OVERFLOW <= '1'; --RCO
+                end if; -- incr
             else                t_cnt <= t_cnt; -- const
             end if;
          end if;
@@ -31,5 +36,6 @@ begin
    end process;
 
    COUNT <= t_cnt; 
+   RCO <= OVERFLOW;
 
 end Behavioral; 
